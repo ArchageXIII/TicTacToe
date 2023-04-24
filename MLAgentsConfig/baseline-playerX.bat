b20d597374ec9632f6f4941e66c7a14a09b09cf2
ArchageXIII <mark.3dwards@gmail.com>
2023-04-24 20:06:26 +0100

    brainupdate
    updated brain

--- MLAgentsConfig/baseline-playerX.bat
+++ MLAgentsConfig/baseline-playerX.bat
@@ -5,6 +5,6 @@ if errorlevel 1 (
 ) else (
     taskkill /f /im tictactoe.exe
 )
-start /wait "" "mlagents-learn" "baseline.playerX.yaml" "--seed=0" "--run-id=baseline-playerX_Round3" "--initialize-from=baseline-playerX_Round2" "--env=D:\DNO\unity_assets\tictactoe\build" "--num-envs=10" "--no-graphics" "--time-scale=20"
+start /wait "" "mlagents-learn" "baseline.playerX.yaml" "--seed=0" "--run-id=baseline-playerX_Round4" "--initialize-from=baseline-playerX_Round3" "--env=D:\TicTacToe\Build" "--num-envs=10" "--no-graphics" "--time-scale=20"
 
 #--initialize-from=lander3d_04 
